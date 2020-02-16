ab436cff7ef34211fdc355c5b1e0d89bcaa2ea61
JunhyunJunhyun <sji010900@gmail.com>
2020-02-16 18:27:50 +0900

    Q3-2

--- Question/Junhyun/3-2.py
+++ Question/Junhyun/3-2.py
@@ -1,1 +1,12 @@
 # while문을 사용해 1부터 1000까지의 자연수 중 3의 배수의 합을 구해 보자.
+
+result = 0
+count = 1
+
+while count <= 1000:
+    if count % 3 == 0: # 3으로 나누어 떨어지는 수는 3의 배수
+        result += count
+    
+    count += 1 
+
+print(result) # 166833 출력
