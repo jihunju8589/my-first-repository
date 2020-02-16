e0d4413b3b1bc9d1fba4fd40fb6c5ba00b4e0617
JunhyunJunhyun <sji010900@gmail.com>
2020-02-16 19:13:27 +0900

    aaa

--- Question/Junhyun/4-2.py
+++ Question/Junhyun/4-2.py
@@ -1,3 +1,13 @@
 # 입력으로 들어오는 모든 수의 평균 값을 계산해 주는 함수를 작성해 보자. (단 입력으로 들어오는 수의 개수는 정해져 있지 않다.)
 
 # ※ 평균 값을 구할 때 len 함수를 사용해 보자.
+
+def add_many(*args):
+    sum=0
+
+    for i in args:
+        sum+=i
+    
+    return sum/len(args)
+
+print(add_many(1, 2, 3, 4))
