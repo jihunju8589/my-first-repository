b1bb2d28276fa171917d409e6fad3d8543da03e5
jihunju8589 <jihunju@gmail.com>
2020-02-16 19:37:53 +0900

    pp

--- Question/Jihun/4-5.py
+++ Question/Jihun/4-5.py
@@ -8,3 +8,10 @@
 
 
 # 이 프로그램은 우리가 예상한 "Life is too short"라는 문장을 출력하지 않는다. 우리가 예상한 값을 출력할 수 있도록 프로그램을 수정해 보자.
+f1 = open("test.txt", 'w')
+f1.write("Life is too short")
+f1.close()
+
+f2 = open("test.txt", 'r')
+print(f2.read())
+f2.close()
