a662cc28e84c548097e37f3cacdf3f9d7617fb7a
JunhyunJunhyun <sji010900@gmail.com>
2020-02-23 13:09:27 +0900

    폴더 구조 변경

--- Algo/Jihun/BJ/10757.py
+++ Algo/Jihun/BJ/10757.py
@@ -1,1 +1,6 @@
 # https://www.acmicpc.net/problem/10757
+#성공!
+A, B=input().split()
+A=int(A)
+B=int(B)
+print(A+B)
--- Algo/Junhyun/BJ/10757.py
+++ Algo/Junhyun/BJ/10757.py
@@ -1,6 +1,1 @@
 # https://www.acmicpc.net/problem/10757
-#성공!
-A, B=input().split()
-A=int(A)
-B=int(B)
-print(A+B)
