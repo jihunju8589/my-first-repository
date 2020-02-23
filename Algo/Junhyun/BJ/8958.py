55eceb988e1eeb660dfd3352f8716b617ed4ddf7
JunhyunJunhyun <sji010900@gmail.com>
2020-02-23 13:36:31 +0900

    예쁘게 변경 22

--- Algo/Junhyun/BJ/8958.py
+++ Algo/Junhyun/BJ/8958.py
@@ -1,1 +1,43 @@
-# https://www.acmicpc.net/problem/8958
+# # https://www.acmicpc.net/problem/8958
+
+# num = int(input())
+
+
+# def scores(testcase):
+#     x = 0
+
+#     while x < testcase:
+#         s = input()
+
+#         if s[0] == "O":
+#             score = 1
+
+#             for i in s:
+#                 if i == "O":
+#                     if i == i + 1:
+#                         score += score + 1
+#                     elif i != i + 1:
+#                         score += 1
+
+#         elif s[0] == "X":
+#             score = 0
+
+#             for i in s:
+#                 if i == "O":
+#                     if i == i + 1:
+#                         score += score + 1
+#                     elif i != i + 1:
+#                         score += 1
+
+#         return score
+
+#         x += 1
+
+
+# print(scores(num))
+
+
+s = "OOXXOXXOOO"  # 1 2 0 0 1 0 0 1 2 3
+total = int()
+score = int()
+
