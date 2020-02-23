403947ca4841c89229af7a655c76b7deb280a459
jihunju8589 <61106737+jihunju8589@users.noreply.github.com>
2020-02-23 16:03:56 +0900

    ㅗ

--- Algo/Jihun/BJ/1789.py
+++ Algo/Jihun/BJ/1789.py
@@ -1,1 +1,23 @@
 # https://www.acmicpc.net/problem/1789
+S=int(input())
+for i in range(1, S):
+    a+=i
+    if a==S:
+        print i
+        break
+    else:
+        def same(S):
+            for i in range(1,S):
+                for x in range(1,S):
+                    b+=i+x
+                    if b==S:
+                        print i
+                        break
+                    else:
+                        same(S)
+
+s=int(input())
+n=1
+while n*n(n+1)/2<=s:
+    n+=1
+print(n-1)        
--- Algo/Jihun/BJ/2480.py
+++ Algo/Jihun/BJ/2480.py
@@ -1,1 +1,12 @@
 # https://www.acmicpc.net/problem/2480
+a = list(map(int, input().split()))
+if a[0]==a[1]==a[2]:
+    print(10000+a[0]*1000)
+elif a[0]==a[1]:
+    print(1000+a[0]*100)
+elif a[1]==a[2]: 
+    print(1000+a[1]*100)
+elif a[0]==a[2]:
+    print(1000+a[0]*100)
+elif a[0]!=a[1] and a[1]!=a[2]:
+    print(max(a)*100)
