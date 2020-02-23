185356c59a8c41894162349e5f47ff057b60ae3e
JunhyunJunhyun <sji010900@gmail.com>
2020-02-23 13:21:14 +0900

    예쁘게 변경

--- Algo/Jihun/BJ/8958.py
+++ Algo/Jihun/BJ/8958.py
@@ -1,26 +1,31 @@
 # https://www.acmicpc.net/problem/8958
-T=int(input())
+
+T = int(input())
+
+
 def scores(T):
-    x=0
-    while(x<T):
-        s=input()
-        if s[0]=='O':
-            score=1
+    x = 0
+    while x < T:
+        s = input()
+        if s[0] == "O":
+            score = 1
             for i in s:
-                if i=='O':
-                    if i==i+1:
-                        score+=score+1
-                    elif i!=i+1:
-                        score+=1
-        elif s[0]=='X':
-            score=0
+                if i == "O":
+                    if i == i + 1:
+                        score += score + 1
+                    elif i != i + 1:
+                        score += 1
+        elif s[0] == "X":
+            score = 0
             for i in s:
-                if i=='O':
-                    if i==i+1:
-                        score+=score+1
-                    elif i!=i+1:
-                        score+=1
+                if i == "O":
+                    if i == i + 1:
+                        score += score + 1
+                    elif i != i + 1:
+                        score += 1
         return score
-    x+=1
-print(scores(T))               
+    x += 1
+
+
+print(scores(T))
 
