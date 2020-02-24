7fa106563e13ec4b54b53b20ac9b60e96b5b9909
jihunju8589 <61106737+jihunju8589@users.noreply.github.com>
2020-02-24 18:55:10 +0900

    jj

--- Algo/Jihun/BJ/4101.py
+++ Algo/Jihun/BJ/4101.py
@@ -1,1 +1,21 @@
 # https://www.acmicpc.net/problem/4101
+# s=[]
+# x=0
+# while(1):    
+#     s[x], s[x+1]=map(int, input().split())
+#     if s[x]==s[x+1]==0:
+#         break
+#     x+=2
+# print(s[3])
+#아몰랑!
+while(1):
+    a, b=map(int, input().split())
+    if a==b==0:
+        break
+if a>b:
+    print("Yes")
+elif a==b==0:
+    print(" ")
+else print("No")
+
+    
--- Algo/Jihun/BJ/9498.py
+++ Algo/Jihun/BJ/9498.py
@@ -1,1 +1,13 @@
 # https://www.acmicpc.net/problem/9498
+#성공!
+t=int(input())
+if t>=90 and t<=100:
+    print("A")
+elif t>=80:
+    print("B")
+elif t>=70:
+    print("C")
+elif t>=60:
+    print("D")
+else:
+    print("F")
