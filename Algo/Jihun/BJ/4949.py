af6e107d44a80c7a691a6339fc069e9fe5351bd0
jihunju8589 <61106737+jihunju8589@users.noreply.github.com>
2020-02-25 13:46:16 +0900

    jhgfd

--- Algo/Jihun/BJ/4949.py
+++ Algo/Jihun/BJ/4949.py
@@ -1,1 +1,26 @@
 # https://www.acmicpc.net/problem/4949
+x1=x2=0
+y1=y2=0
+t=[]
+while(1):
+    s=input()
+    for i in s:
+        if i=="(":
+            x1+=1
+        if i==")":
+            x2+=1
+        if i=="[":
+            y1+=1
+        if i=="]":
+            y2+=1
+    if x1==x2 and y1==y2:
+        t.append(0)
+    else:
+        t.append(1)
+    if(s=="."):
+        break
+for i in t:
+    if i==0:
+        print("yes")
+    else:
+        print("no")
--- Algo/Junhyun/BJ/10817.py
+++ Algo/Junhyun/BJ/10817.py
@@ -1,1 +1,4 @@
 # https://www.acmicpc.net/problem/10817
+s=list(map(int, input().split()))
+s.sort()
+print(s[1])
