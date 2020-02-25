1c72ff4a106a0c5cd56bbde793b151894b9fc181
JunhyunJunhyun <sji010900@gmail.com>
2020-02-25 10:16:45 +0900

    문제 첨삭

--- Algo/Jihun/BJ/2935.py
+++ Algo/Jihun/BJ/2935.py
@@ -1,9 +1,9 @@
 # https://www.acmicpc.net/problem/2935
-#정답!
-A=int(input())
-x=input()
-B=int(input())
-if x=='+':
-    print(A+B)
+# 정답!
+A = int(input())
+x = input()
+B = int(input())
+if x == "+":
+    print(A + B)
 else:
-    print(A*B)
+    print(A * B)
