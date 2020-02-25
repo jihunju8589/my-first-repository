bd72bb396a9fe4f3c9af4d3226db67504124dc4f
JunhyunJunhyun <sji010900@gmail.com>
2020-02-25 16:39:01 +0900

    문제 추가

---
+++ Algo/Junhyun/BJ/1924.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/1924
+
+import calendar
+
+day = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+a, b = map(int, input().split())
+
+k = calendar.weekday(2007, a, b)
+
+print(day[k])
---
+++ Algo/Junhyun/BJ/2292.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/2292
+
+room = int(input())
+
+if room == 1:
+    print(1)
+else:
+    num = 1
+    answer = 1
+
+    while room >= 2:
+        room -= num * 6
+        num += 1
+        answer += 1
+
+    print(answer)
