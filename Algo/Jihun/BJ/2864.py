b7f0ffc8ac01edac2a26f942a64461b6dbd04ed9
jihunju8589 <61106737+jihunju8589@users.noreply.github.com>
2020-02-25 11:55:24 +0900

    sdfgf

--- Algo/Jihun/BJ/2864.py
+++ Algo/Jihun/BJ/2864.py
@@ -1,9 +1,7 @@
 # https://www.acmicpc.net/problem/2864
-A, B=map(int, input().split())
-a=[]
-b=[]
-a.append(A)
-print(a[2])
-b.append(B)
-len(a)
-len(b)
+A, B=input().split()
+replaceAll1=int(A.replace("5", "6"))
+replaceAll2=int(B.replace("5", "6"))
+replaceAll3=int(A.replace("6", "5"))
+replaceAll4=int(B.replace("6", "5"))
+print(replaceAll3+replaceAll4, replaceAll1+replaceAll2)
