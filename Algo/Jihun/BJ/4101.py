05f2d0c1489a1e84b79015178f25a696dcdab585
JunhyunJunhyun <sji010900@gmail.com>
2020-02-25 10:18:01 +0900

    문제 첨삭

--- Algo/Jihun/BJ/4101.py
+++ Algo/Jihun/BJ/4101.py
@@ -1,22 +1,21 @@
 # https://www.acmicpc.net/problem/4101
-s=[]
-x=0
-while(1):
-    a, b=map(int, input().split())
+s = []
+x = 0
+
+while 1:
+    a, b = map(int, input().split())
     s.append(a)
     s.append(b)
-    x+=1
-    if a==b==0:
+    x += 1
+    if a == b == 0:
         break
-for i in range(0,x,2):
-    if s[i]>s[i+1]:
+
+for i in range(0, x, 2):
+    if s[i] > s[i + 1]:
         print("Yes")
-    elif s[i]==s[i+1]==0:
+    elif s[i] == s[i + 1] == 0:
         break
     else:
         print("No")
-#for(i=0;i<x;i+=2) 이렇게 i가 2씩 커지게 하고 싶은데 어떻게 해?ㅠ
 
-
-
-    
+# for(i=0;i<x;i+=2) 이렇게 i가 2씩 커지게 하고 싶은데 어떻게 해?ㅠ
--- Algo/Junhyun/BJ/4101.py
+++ Algo/Junhyun/BJ/4101.py
@@ -1,1 +1,22 @@
 # https://www.acmicpc.net/problem/4101
+
+s = []
+x = 0
+
+while 1:
+    a, b = map(int, input().split())
+    s.append(a)
+    s.append(b)
+    x += 1
+    if a == b == 0:
+        break
+
+for i in range(0, x, 2):
+    if s[i] > s[i + 1]:
+        print("Yes")
+    elif s[i] == s[i + 1] == 0:
+        break
+    else:
+        print("No")
+
+# for(i=0;i<x;i+=2) 이렇게 i가 2씩 커지게 하고 싶은데 어떻게 해?ㅠ
