3c696fb1439132469df3f3cf3b0814b21b90533f
JunhyunJunhyun <sji010900@gmail.com>
2020-02-25 13:50:10 +0900

    문제 첨삭

--- Algo/Jihun/BJ/4949.py
+++ Algo/Jihun/BJ/4949.py
@@ -1,26 +1,28 @@
 # https://www.acmicpc.net/problem/4949
-x1=x2=0
-y1=y2=0
-t=[]
-while(1):
-    s=input()
+x1 = x2 = 0
+y1 = y2 = 0
+t = []
+
+while 1:
+    s = input()
     for i in s:
-        if i=="(":
-            x1+=1
-        if i==")":
-            x2+=1
-        if i=="[":
-            y1+=1
-        if i=="]":
-            y2+=1
-    if x1==x2 and y1==y2:
+        if i == "(":
+            x1 += 1
+        if i == ")":
+            x2 += 1
+        if i == "[":
+            y1 += 1
+        if i == "]":
+            y2 += 1
+    if x1 == x2 and y1 == y2:
         t.append(0)
     else:
         t.append(1)
-    if(s=="."):
+    if s == ".":
         break
+
 for i in t:
-    if i==0:
+    if i == 0:
         print("yes")
     else:
         print("no")
