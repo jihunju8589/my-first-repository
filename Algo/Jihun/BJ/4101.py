3f4db90aa034ad1bd8fdbb465e2451ef3ec1c268
jihunju8589 <61106737+jihunju8589@users.noreply.github.com>
2020-02-25 10:24:30 +0900

    dsf

--- Algo/Jihun/BJ/4101.py
+++ Algo/Jihun/BJ/4101.py
@@ -1,4 +1,5 @@
 # https://www.acmicpc.net/problem/4101
+#성공!
 s = []
 x = 0
 
@@ -10,7 +11,7 @@ while 1:
     if a == b == 0:
         break
 
-for i in range(0, x, 2):
+for i in range(0, x*2, 2):
     if s[i] > s[i + 1]:
         print("Yes")
     elif s[i] == s[i + 1] == 0:
