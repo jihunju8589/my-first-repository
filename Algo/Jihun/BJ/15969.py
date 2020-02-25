1a8b17fb834adef4505c61d1e7f6c9ddae257d6a
jihunju8589 <61106737+jihunju8589@users.noreply.github.com>
2020-02-25 11:20:59 +0900

    sdf

--- Algo/Jihun/BJ/15969.py
+++ Algo/Jihun/BJ/15969.py
@@ -1,1 +1,6 @@
 # https://www.acmicpc.net/problem/15969
+#성공!
+N=int(input())
+s=list(map(int, input().split()))
+s.sort()
+print(s[N-1]-s[0])
--- Algo/Jihun/BJ/2864.py
+++ Algo/Jihun/BJ/2864.py
@@ -1,1 +1,9 @@
 # https://www.acmicpc.net/problem/2864
+A, B=map(int, input().split())
+a=[]
+b=[]
+a.append(A)
+print(a[2])
+b.append(B)
+len(a)
+len(b)
