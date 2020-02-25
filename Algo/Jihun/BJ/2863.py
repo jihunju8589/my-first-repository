d6caef23d5aa4ec826b4a1d5758b2921c94a7258
jihunju8589 <61106737+jihunju8589@users.noreply.github.com>
2020-02-25 12:33:44 +0900

    asdffgfghn

--- Algo/Jihun/BJ/2863.py
+++ Algo/Jihun/BJ/2863.py
@@ -1,1 +1,15 @@
 # https://www.acmicpc.net/problem/2863
+s1=list(map(int, input().split()))
+s2=list(map(int, input().split()))
+s3=[]
+s3=s1+s2
+total=[]
+total.append(s3[0]/s3[2]+s3[1]/s3[3])
+for i in range(0,4):
+    temp=s3[3]
+    s3[3]=s3[2]
+    s3[2]=s3[1]
+    s3[1]=s3[0]
+    s3[0]=temp
+    total.append(s3[0]/s3[2]+s3[1]/s3[3])
+print(max(total))
