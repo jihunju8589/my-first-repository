87aa5feac019d23d5483af289f6b83ed469caba3
jihunju8589 <61106737+jihunju8589@users.noreply.github.com>
2020-03-03 10:31:13 +0900

    wrt

--- Algo/Jihun/BJ/11726.py
+++ Algo/Jihun/BJ/11726.py
@@ -1,2 +1,4 @@
 # https://www.acmicpc.net/problem/11726
+#2020.03.01.수업.
+n=int(input())
 
--- Algo/Jihun/BJ/6603.py
+++ Algo/Jihun/BJ/6603.py
@@ -4,3 +4,5 @@ while(1):
     line=list(map(int, input().split()))
     if line[0]==0:
         break
+    result=[]
+    
